e0da7999deed0c43acbd945bccefcddf5ee9a1a5
EasoDongH <easondongh@qq.com>
2019-05-16 10:44:47 +0800

    设计模式之装饰器模式

---
+++ SoftwareEngineering/DesignPattern/3. DecoratorPattern/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.DecoratorPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person xc = new Person("小菜");
+            Console.WriteLine("第一种打扮：");
+            Sneakers pqx = new Sneakers();
+            Trouser kk = new Trouser();
+            TShirts dtx = new TShirts();
+
+            pqx.Decorate(xc);
+            kk.Decorate(pqx);
+            dtx.Decorate(kk);
+            dtx.Show();
+
+            Console.Read();
+        }
+    }
+
+    class Person
+    {
+        public Person() { }
+
+        private string name;
+        public Person(string name)
+        {
+            this.name = name;
+        }
+
+        public virtual void Show()
+        {
+            Console.WriteLine("装扮的{0}", name);
+        }
+    }
+
+    class Finery : Person
+    {
+        protected Person component;
+        public void Decorate(Person component)
+        {
+            this.component = component;
+        }
+        public override void Show()
+        {
+            if (component != null)
+                component.Show();
+        }
+    }
+
+    class TShirts : Finery
+    {
+        public override void Show()
+        {
+            Console.Write("T恤    ");
+            base.Show();
+        }
+    }
+
+    class Sneakers : Finery
+    {
+        public override void Show()
+        {
+            Console.Write("球鞋    ");
+            base.Show();
+        }
+    }
+
+    class Trouser : Finery
+    {
+        public override void Show()
+        {
+            Console.Write("裤子    ");
+            base.Show();
+        }
+    }
+}
