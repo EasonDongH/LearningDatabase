91b3210ad9e655fed1eafbd36ab86e03d0523ecd
EasonDongH <easondongh@qq.com>
2019-08-09 21:53:42 +0800

    添加压缩与解压缩文件。

---
+++ CSharp/压缩与解压缩/TestSharpZipLib/TestSharpZipLib/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TestSharpZipLib
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SharpZipLibHelper.ZipDecompress(@"C:\Users\EasonDongH\Desktop\新建文件夹 (2)", "Python Cookbook第三版中文.zip", @"C:\Users\EasonDongH\Desktop\新建文件夹 (3)");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SharpZipLibHelper.ZipCompress(new string[] { @"C:\Users\EasonDongH\Desktop\123.txt", @"C:\Users\EasonDongH\Desktop\234.txt", @"C:\Users\EasonDongH\Desktop\新建文件夹 (3)" }, @"C:\Users\EasonDongH\Desktop\新建文件夹\123");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SharpZipLibHelper.ZipCompress(@"C:\Users\EasonDongH\Desktop\新建文件夹 (3)", @"C:\Users\EasonDongH\Desktop\123.zip");
+        }
+    }
+}
