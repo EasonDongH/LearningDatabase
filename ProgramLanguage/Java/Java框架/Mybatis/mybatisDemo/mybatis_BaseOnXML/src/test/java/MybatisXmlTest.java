11617532b10f6f675d071be7b967d8f4e903b488
EasonDongH <easondongh@qq.com>
2019-12-27 17:03:01 +0800

    更新mybatisDemo.

---
+++ ProgramLanguage/Java/Java框架/Mybatis/mybatisDemo/mybatis_BaseOnXML/src/test/java/MybatisXmlTest.java
@@ -0,0 +1,23 @@
+
+import com.easondongh.domain.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MybatisXmlTest {
+
+    @Test
+    public void testSelectOne() throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        User user = sqlSession.selectOne("com.easondongh.UserMapper.selectUser", 1);
+        System.out.println(user);
+    }
+}
